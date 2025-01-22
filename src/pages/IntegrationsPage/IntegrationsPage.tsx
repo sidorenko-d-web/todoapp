f213b9a9e34e2b8e0002d6c74e9745fe943b0396
Kiryl Filipovich <fkiryl@gmail.com>
2025-01-22 22:18:14 +0300

    started implementing integrations page

--- src/pages/IntegrationsPage/IntegrationsPage.tsx
+++ src/pages/IntegrationsPage/IntegrationsPage.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 import styles from './IntegrationsPage.module.scss';
 
 export const IntegrationsPage: React.FC = () => {
-    return (<div>
+    return (
+    <div className={styles.wrp}>
         <h1 className={styles.pageTitle}>Интеграции</h1>
         <div className={styles.statsUnderTitle}>
-            <p>856,754 млн.</p>
-            <p>223 567</p>
+            <p className={styles.stat}>856,754 млн.</p>
+            <p className={styles.stat}>223 567</p>
+        </div>
+        <div className={styles.integrationNameWrp}>
+            <p className={styles.integrationTitle}>Интеграция 3</p>
+            <p className={styles.integrationLevel}>Brilliant</p>
         </div>
     </div>);
 }
