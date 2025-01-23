9caac04c489fd15fe9b0dfff628edbe4d5c18f6c
Kiryl Filipovich <fkiryl@gmail.com>
2025-01-23 12:02:12 +0300

    creating layout for integrations page

--- src/pages/IntegrationsPage/IntegrationsPage.tsx
+++ src/pages/IntegrationsPage/IntegrationsPage.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import styles from './IntegrationsPage.module.scss';
 
+import subscribersIcon from '../../assets/icons/subscribers.svg';
+import viewsIcon from '../../assets/icons/views.svg';
+import coinIcon from '../../assets/icons/coin.svg';
+import brilliantIcon from '../../assets/icons/integration_brilliant.svg';
+
 export const IntegrationsPage: React.FC = () => {
     return (
         <div className={styles.wrp}>
@@ -9,14 +14,24 @@ export const IntegrationsPage: React.FC = () => {
             <div className={styles.statsUnderTitleWrp}>
                 <div className={styles.toCenterStats}/>
                 <div className={styles.statsUnderTitle}>
-                    <p className={styles.stat}>856,754 млн.</p>
-                    <p className={styles.stat}>223 567</p>
+                    <div className={styles.statWrp}>
+                        <p className={styles.stat}>856,754 млн. </p>
+                        <img src={viewsIcon} height={12} width={12}/>
+                    </div>
+                    
+                    <div className={styles.statWrp}>
+                        <p className={styles.stat}>223 567 </p>
+                        <img src={subscribersIcon} height={12} width={12}/>
+                    </div>
                 </div>
                 <button className={styles.seeStatsButton}></button>
             </div>
             <div className={styles.integrationNameWrp}>
                 <p className={styles.integrationTitle}>Интеграция 3</p>
-                <p className={styles.integrationLevel}>Brilliant</p>
+                <div className={styles.integrationLevelWrp}>
+                    <p className={styles.integrationLevel}>Brilliant</p>
+                    <img src={brilliantIcon}/>
+                </div>
             </div>
 
             <div className={styles.integration}>
@@ -26,15 +41,24 @@ export const IntegrationsPage: React.FC = () => {
             <div className={styles.integrationStatsWrp}>
                 <div className={styles.integrationStat}>
                     <p className={styles.amount}>223 567</p>
-                    <p className={styles.type}>Подписчики</p>
+                    <div className={styles.typeWrp}>
+                        <img src={subscribersIcon}/>
+                        <p className={styles.type}>  Подписчики</p>
+                    </div>
                 </div>
                 <div className={styles.integrationStat}>
                     <p className={styles.amount}>856,754 млн.</p>
-                    <p className={styles.type}>Просмотры</p>
+                    <div className={styles.typeWrp}>
+                        <img src={viewsIcon}/>
+                        <p className={styles.type}>Просмотры</p>
+                    </div>
                 </div>
                 <div className={styles.integrationStat}>
                     <p className={styles.amount}>223 567</p>
-                    <p className={styles.type}>Доход</p>
+                    <div className={styles.typeWrp}>
+                        <img src={coinIcon}/>
+                        <p className={styles.type}>Доход</p>
+                    </div>
                 </div>
             </div>
         </div>);
