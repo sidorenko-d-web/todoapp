3c49b38dd4ecdd6e56c766a15b2bcc7ffe9a44e8
marketerKA <karhipov606@gmail.com>
2025-01-27 02:20:54 +0300

    Update: Part #3 lang page style

--- src/pages/LanguageSelect/LanguageSelect.tsx
+++ src/pages/LanguageSelect/LanguageSelect.tsx
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 import styles from './LanguageSelect.module.scss';
+import checkIcon from '../../../src/assets/icons/checkmark-in-the-circle.svg';
+import circleIcon from '../../../src/assets/icons/circle-blue.svg';
+
+const LANGUAGES = [
+  { id: 'en', flag: '/us-flag.svg', label: 'English language' },
+  { id: 'ru', flag: '/ru-flag.svg', label: 'Русский язык' },
+];
 
 export const LanguageSelect = () => {
   const [selectedLang, setSelectedLang] = useState('en');
@@ -7,27 +14,24 @@ export const LanguageSelect = () => {
   return (
     <div className={styles.root}>
       <div className={styles.languageOptions}>
-        <div
-          className={`${styles.option} ${selectedLang === 'en' ? styles.selected : ''}`}
-          onClick={() => setSelectedLang('en')}
-        >
-          <img src="/us-flag.svg" alt="US" />
-          <span>English language</span>
-          <div className={styles.check} />
-        </div>
-
-        <div
-          className={`${styles.option} ${selectedLang === 'ru' ? styles.selected : ''}`}
-          onClick={() => setSelectedLang('ru')}
-        >
-          <img src="/ru-flag.svg" alt="RU" />
-          <span>Русский язык</span>
-          <div className={styles.check} />
-        </div>
+        {LANGUAGES.map(({ id, flag, label }) => (
+          <div
+            key={id}
+            className={`${styles.option} ${selectedLang === id ? styles.selected : ''}`}
+            onClick={() => setSelectedLang(id)}
+          >
+            <div className={styles.flagWrapper}>
+              <img src={flag} alt={id.toUpperCase()} className={styles.flag} />
+            </div>
+            <span className={selectedLang === id ? styles.selectedText : ''}>{label}</span>
+            <div className={styles.selectWrapper}>
+              <img src={selectedLang === id ? checkIcon : circleIcon} className={styles.icon} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
-
       <button className={styles.continueButton}>Продолжить/Continue</button>
-      <div className={styles.selectText}>Выберите язык | Select language</div>
+      <div className={styles.selectText}>Выберите язык&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Select language</div>
     </div>
   );
 };
