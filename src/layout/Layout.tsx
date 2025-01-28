67ed2334ce56ae75b371eb734e49626536e1291e
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-01-28 21:33:50 +0300

    Added header, fixed content height from header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Footer } from '../components/Footer';
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.scss'
+import { Header } from '../components/Header/';
 
 interface LayoutProps {
 
@@ -12,6 +13,7 @@ const Layout: FC<LayoutProps> = ({}: LayoutProps) => {
 
   return (
     <div className={styles.wrp}>
+      <Header />
       <main className={styles.content}>
         <Outlet/>
       </main>
