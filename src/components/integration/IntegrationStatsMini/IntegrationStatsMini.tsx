72eecfc2e5d7fdc8f2488d50b8a717bbdcf364c9
DeHopen <a2302004b23@yandex.ru>
2025-01-29 15:40:34 +0300

    Fix: connect api, styles and functions

--- src/components/integration/IntegrationStatsMini/IntegrationStatsMini.tsx
+++ src/components/integration/IntegrationStatsMini/IntegrationStatsMini.tsx
@@ -4,6 +4,7 @@ import styles from './IntegrationStatsMini.module.scss';
 
 import viewsIcon from '../../../assets/icons/views.svg';
 import subscribersIcon from '../../../assets/icons/subscribers.svg';
+import { useNavigate } from 'react-router-dom';
 
 
 interface IntegrationStatsMiniProps {
@@ -13,21 +14,22 @@ interface IntegrationStatsMiniProps {
 
 
 export const IntegrationStatsMini:React.FC<IntegrationStatsMiniProps> = ({views, subscribers}) => {
-    return (
+  const navigate = useNavigate();
+  return (
         <div className={styles.statsUnderTitleWrp}>
             <div className={styles.toCenterStats} />
             <div className={styles.statsUnderTitle}>
                 <div className={styles.statWrp}>
                     <p className={styles.stat}>{views}</p>
-                    <img src={viewsIcon} height={12} width={12} />
+                    <img src={viewsIcon} height={12} width={12} alt=''/>
                 </div>
 
                 <div className={styles.statWrp}>
                     <p className={styles.stat}>{subscribers}</p>
-                    <img src={subscribersIcon} height={12} width={12} />
+                    <img src={subscribersIcon} height={12} width={12} alt=''/>
                 </div>
             </div>
-            <button className={styles.seeStatsButton}></button>
+            <button onClick={() => navigate(`/integrations`)} className={styles.seeStatsButton}></button>
         </div>
     )
 }
