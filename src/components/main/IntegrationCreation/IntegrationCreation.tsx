08c6c2e7eef44252f152ee0ca02a651ee0add580
Valeriy <masterbox06@gmail.com>
2025-01-29 15:32:11 +0400

    feat: create subscription buying modal

--- src/components/main/IntegrationCreation/IntegrationCreation.tsx
+++ src/components/main/IntegrationCreation/IntegrationCreation.tsx
@@ -1,12 +1,16 @@
 import integrationIcon from '../../../assets/icons/integration.svg';
 import { useModal } from '../../../hooks';
 import { MODALS } from '../../../constants/modals.ts';
-import { useGetCurrentUserProfileInfoQuery, useGetIntegrationsQuery } from '../../../redux';
+import { profileApi, useGetCurrentUserProfileInfoQuery, useGetIntegrationsQuery } from '../../../redux';
 import { IntegrationCreationCard, IntegrationCreationModal } from '../';
+import { SubscribeModal, SuccessfullySubscribedModal } from '../../';
+import { useDispatch } from 'react-redux';
 
 import s from './IntegrationCreation.module.scss';
 
 export const IntegrationCreation = () => {
+  const dispatch = useDispatch();
+
   const { data: profile } = useGetCurrentUserProfileInfoQuery();
   const {
     data: integrations,
@@ -15,24 +19,52 @@ export const IntegrationCreation = () => {
     pollingInterval: 10 * 60 * 1000,
   });
   const { openModal, closeModal } = useModal();
-  console.log(integrationsError);
+
+  const handleIntegrationCreation = () => {
+    if (!profile) return;
+
+    if (profile?.subscription_integrations_left <= 0) {
+      openModal(MODALS.SUBSCRIBE);
+      return;
+    }
+
+    openModal(MODALS.CREATING_INTEGRATION);
+  };
+
+  const handleSuccessfullySubscribed = () => {
+    closeModal(MODALS.SUBSCRIBE);
+    dispatch(profileApi.util.invalidateTags([ 'Me' ]));
+    openModal(MODALS.SUCCESSFULLY_SUBSCRIBED);
+  };
+
   return (
     <section className={s.integrationsControls}>
-      <button className={s.button} onClick={() => openModal(MODALS.CREATING_INTEGRATION)}>
+      <button className={s.button} disabled={!profile} onClick={handleIntegrationCreation}>
         Создать интеграцию
         <span className={s.buttonBadge}>
           {profile?.subscription_integrations_left || 0}/5 <img src={integrationIcon} height={12} width={12}
-                                                                  alt="integration" />
+                                                                alt="integration" />
         </span>
       </button>
-      <IntegrationCreationModal modalId={MODALS.CREATING_INTEGRATION}
-                                onClose={() => closeModal(MODALS.CREATING_INTEGRATION)} />
-
       {
         integrationsError?.status === 404 ? null : integrations?.integrations.map(integration => (
           <IntegrationCreationCard key={integration.id} integration={integration} />
         ))
       }
+
+      <IntegrationCreationModal
+        modalId={MODALS.CREATING_INTEGRATION}
+        onClose={() => closeModal(MODALS.CREATING_INTEGRATION)}
+      />
+      <SubscribeModal
+        modalId={MODALS.SUBSCRIBE}
+        onClose={() => closeModal(MODALS.SUBSCRIBE)}
+        onSuccess={handleSuccessfullySubscribed}
+      />
+      <SuccessfullySubscribedModal
+        modalId={MODALS.SUCCESSFULLY_SUBSCRIBED}
+        onClose={() => closeModal(MODALS.SUCCESSFULLY_SUBSCRIBED)}
+      />
     </section>
   );
 };
