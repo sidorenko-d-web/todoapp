0c3e1766ce870d249db3106942ffbf3be07283ff
Kiryl Filipovich <fkiryl@gmail.com>
2025-01-30 19:26:43 +0300

    wardrobe info fix

--- src/components/wardrobe/WardrobeInfo/WardrobeInfo.tsx
+++ src/components/wardrobe/WardrobeInfo/WardrobeInfo.tsx
@@ -3,38 +3,38 @@ import styles from './WardrobeInfo.module.scss';
 import { Link } from 'react-router-dom';
 
 import skinPlaceholder from '../../../assets/icons/skin-placeholder.svg';
-import coin from '../../../assets/icons/coin.svg';
 import statusInactive from '../../../assets/icons/status-inactive.svg';
+import { useGetInventorySkinsQuery } from '../../../redux/api/inventory/api';
 
 export const WardrobeInfo: React.FC = () => {
+
+    const { data } = useGetInventorySkinsQuery();
+
+
     return (
-        <div className={styles.wrp}>
-            <Link to='/profile'>
-                <button className={styles.backBtn}/>
-            </Link>
+        <>
+            {data && <div className={styles.wrp}>
+                <Link to='/profile'>
+                    <button className={styles.backBtn} />
+                </Link>
 
-            <div className={styles.infoWrp}>
-                <h1 className={styles.title}>Гардероб</h1>
-                <div className={styles.statsWrp}>
-                    <div className={styles.statWrp}>
-                        <span className={styles.stat}>12</span>
-                        <img src={skinPlaceholder} width={14} height={14}/>
-                    </div>
+                <div className={styles.infoWrp}>
+                    <h1 className={styles.title}>Гардероб</h1>
+                    <div className={styles.statsWrp}>
+                        <div className={styles.statWrp}>
+                            <span className={styles.stat}>{data.count}</span>
+                            <img src={skinPlaceholder} width={14} height={14} />
+                        </div>
 
-                    <div className={styles.statWrp}>
-                        <span className={styles.stat}>+1</span>
-                        <img src={coin} width={14} height={14}/>
-                        <span className={styles.stat}>/сек</span>
+                        <div className={styles.statusWrp}>
+                            <span className={styles.status}>без статуса</span>
+                            <img src={statusInactive} height={14} width={14} />
+                        </div>
                     </div>
                 </div>
 
-                <div className={styles.statusWrp}>
-                    <span className={styles.status}>без статуса</span>
-                    <img src={statusInactive} height={14} width={14}/>
-                </div>
-            </div>
-
-            <div className={styles.toCenterInfo}/>
-        </div>
+                <div className={styles.toCenterInfo} />
+            </div>}
+        </>
     )
 }
