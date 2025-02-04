e76c864e6c2c186403a2758ffe7110e10460f333
Dimon1200 <dimoooon1200@gmail.com>
2025-02-04 23:30:12 +0500

    changes in tabs sort

--- src/pages/ShopPage/ShopPage.tsx
+++ src/pages/ShopPage/ShopPage.tsx
@@ -7,6 +7,26 @@ import { useGetInventoryItemsQuery } from '../../redux/api/inventory/api';
 
 type TypeTab<T> = { title: string; value: T };
 
+const getStatus = (name: string): { status: 'pro' | 'adv' | 'base'; name: string } => {
+  let status: 'pro' | 'adv' | 'base' = 'base';
+  if (name.includes('Pro')) status = 'pro';
+  else if (name.includes('Prem')) status = 'adv';
+  else status = 'base';
+
+  const nameArr = name.split(' ');
+  if (status === 'base') {
+    return {
+      status,
+      name: nameArr.join(' '),
+    };
+  }
+  console.log(nameArr.slice(0, nameArr.length - 1).join(' '));
+  return {
+    status,
+    name: nameArr.slice(0, nameArr.length - 1).join(' '),
+  };
+};
+
 const StorePage: FC = () => {
   const [shopCategory, setShopCategory] = useState<TypeTab<TypeItemCategory>>();
   const [itemsRarity, setItemsQuality] = useState<TypeTab<TypeItemRarity>>();
@@ -27,11 +47,23 @@ const StorePage: FC = () => {
     refetch: refetchInventory,
   } = useGetInventoryItemsQuery({
     item_category: shopCategory?.value as TypeItemCategory,
-    level: 1,
     item_rarity: 'red',
   });
 
-  const items = shop?.items.filter(item => !inventory?.items.map(_item => _item.name).includes(item.name));
+  const items = shop?.items.filter(item => {
+    const { status, name } = getStatus(item.name);
+    const baseInventoryItem = inventory?.items.find(_item => {
+      return (_item.name) === name && getStatus(_item.name).status === 'base';
+    });
+
+    const advInventoryItem = inventory?.items.find(
+      _item => getStatus(_item.name).name === name && getStatus(_item.name).status === 'adv',
+    );
+
+    if (status === 'base') return !baseInventoryItem;
+    else if (status === 'adv') return baseInventoryItem?.level === 14;
+    else return advInventoryItem && advInventoryItem?.level === 14;
+  });
 
   const refetch = () => {
     refetchInventory();
@@ -55,3 +87,4 @@ const StorePage: FC = () => {
 };
 
 export default StorePage;
+
