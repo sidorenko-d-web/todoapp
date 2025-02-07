662806119fc723325980e6e3fd1e3686acd6b0b2
Valeriy <masterbox06@gmail.com>
2025-02-07 16:31:49 +0400

    styles: fix top users modal

--- src/components/promotion/Modal/TopUsers/TopUsers.tsx
+++ src/components/promotion/Modal/TopUsers/TopUsers.tsx
@@ -4,7 +4,8 @@ import clanRed from '../../../../assets/icons/clanRed.svg';
 import s from './TopUsers.module.scss';
 import fire from '../../../../assets/icons/fire.svg';
 import user from '../../../../assets/icons/user.svg';
-import close from '../../../../assets/icons/close.svg';
+import close from '../../../../assets/icons/close-dark.svg';
+import chest from '../../../../assets/icons/chest-purple.svg';
 import classNames from 'classnames';
 import BottomModal from '../../../shared/BottomModal/BottomModal.tsx';
 import { useGetCurrentUserProfileInfoQuery, useGetTopProfilesQuery } from '../../../../redux/index.ts';
@@ -16,15 +17,14 @@ interface InviteFriendProps {
 }
 
 export const TopUsers: FC<InviteFriendProps> = ({
-  modalId,
-  onClose,
-}: InviteFriendProps) => {
+                                                  modalId,
+                                                  onClose,
+                                                }: InviteFriendProps) => {
   const { data } = useGetTopProfilesQuery();
   const topProfiles = data?.profiles || [];
 
-  const { data: userProfileData} = useGetCurrentUserProfileInfoQuery();
-
-
+  const { data: userProfileData } = useGetCurrentUserProfileInfoQuery();
+  const { data: me } = useGetCurrentUserProfileInfoQuery();
 
   const userPosition = userProfileData && topProfiles
     ? topProfiles.findIndex((profile: { id: string; }) => profile.id === userProfileData.id)
@@ -50,7 +50,7 @@ export const TopUsers: FC<InviteFriendProps> = ({
       </ul>
       <ul className={classNames(s.ulBlock, s.blogUsers)}>
         {topProfiles.map((profile, index) => (
-          <Link to={`/profile/${profile.id}`} key={profile.id} className={s.listUser}>
+          <Link to={`/profile/${profile.id}`} key={profile.id} className={classNames(s.listUser, me && me.id === profile.id && s.active)}>
             <div
               className={classNames(s.cardBlock, {/*{ [s.vipCard]: profile.vip }*/ })}>
               <div className={s.card}>
@@ -64,7 +64,7 @@ export const TopUsers: FC<InviteFriendProps> = ({
                   <li className={s.number}>6</li>
                   <li className={s.fireIcon}>
                     <img src={fire} alt="fire" width={12} height={12} />
-                    <span>{profile.points}</span>
+                    <span>{Math.round((+profile.points)).toLocaleString('RU-ru')}</span>
                   </li>
                   {/*{profile.vip &&*/}
                   {/*  <li className={s.vip}>*/}
@@ -74,50 +74,16 @@ export const TopUsers: FC<InviteFriendProps> = ({
                 </ul>
               </div>
               <div className={s.numUser}>
-                {`${index+1}`}<img src={clanRed} height={14} width={14} alt={'clanRed'} />
+                {`${index + 1}`}<img src={clanRed} height={14} width={14} alt={'clanRed'} />
               </div>
             </div>
             <div className={classNames(s.cardBox, {/*{ [s.vipCardBox]: profile.vip }*/ })}>
-              {/*{profile.vip ? <img src={chest} height={20} width={20} alt="chest" /> :*/}
-              <img src={close} alt="close" width={20} height={20} />
-              {/*}*/}
+              {index <= 100 ? <img src={chest} height={20} width={20} alt="chest" /> :
+                <img src={close} alt="close" width={20} height={20} />
+              }
             </div>
           </Link>
-        ))}{topProfiles.map((profile, index) => (
-        <Link to={`/profile/${profile.id}`} key={profile.id} className={s.listUser}>
-          <div
-            className={classNames(s.cardBlock, {/*{ [s.vipCard]: profile.vip }*/ })}>
-            <div className={s.card}>
-              <img src={user} alt="user" width={27} height={36} />
-            </div>
-          </div>
-          <div className={s.userBlock}>
-            <div className={s.userInfo}>
-              <h3 className={s.text}>{profile.username}</h3>
-              <ul className={classNames(s.ulBlock, s.infoRang)}>
-                <li className={s.number}>6</li>
-                <li className={s.fireIcon}>
-                  <img src={fire} alt="fire" width={12} height={12} />
-                  <span>{profile.points}</span>
-                </li>
-                {/*{profile.vip &&*/}
-                {/*  <li className={s.vip}>*/}
-                {/*    <img src={star} alt="star" width={12} height={12} />*/}
-                {/*    <span>VIP</span>*/}
-                {/*  </li>}*/}
-              </ul>
-            </div>
-            <div className={s.numUser}>
-              {`${index + 1}`}<img src={clanRed} height={14} width={14} alt={'clanRed'} />
-            </div>
-          </div>
-          <div className={classNames(s.cardBox, {/*{ [s.vipCardBox]: profile.vip }*/ })}>
-            {/*{profile.vip ? <img src={chest} height={20} width={20} alt="chest" /> :*/}
-            <img src={close} alt="close" width={20} height={20} />
-            {/*}*/}
-          </div>
-        </Link>
-      ))}
+        ))}
       </ul>
     </BottomModal>
   );
