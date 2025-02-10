52cb567c62e9869f6c9e0ee7e119bcd2cd4278d5
Valeriy <masterbox06@gmail.com>
2025-02-10 15:30:56 +0400

    chore: add abbreviation format wrapper at tasks page

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -4,6 +4,7 @@ import coinIcon from '../../assets/icons/coin.png';
 
 import s from './TasksPage.module.scss';
 import { DailyTasks, SocialTasks, TopTasks } from '../../components';
+import { formatAbbreviation } from '../../helpers';
 
 export const TasksPage: React.FC = () => {
   return (
@@ -11,15 +12,16 @@ export const TasksPage: React.FC = () => {
       <section className={s.topSection}>
         <h1 className={s.pageTitle}>Задания</h1>
         <div className={s.badges}>
-          <span className={s.badge}>+100 <img src={coinIcon} height={14} width={14} alt={'income'} />/сек.</span>
-          <span className={s.badge}>+150 <img src={subscribersIcon} height={14} width={14}
-                                              alt={'subscribers'} />/сек.</span>
+          <span className={s.badge}>+{formatAbbreviation(100)} <img src={subscribersIcon} height={14} width={14}
+                                              alt={'subscribers'} /></span>
+          <span className={s.badge}>+{formatAbbreviation(150)} <img src={coinIcon} height={14} width={14} alt={'income'} /></span>
+          <span className={s.badge}>+{formatAbbreviation(1)} <img src={coinIcon} height={14} width={14} alt={'income'} />/сек.</span>
         </div>
       </section>
 
-      <DailyTasks/>
-      <TopTasks/>
-      <SocialTasks/>
+      <DailyTasks />
+      <TopTasks />
+      <SocialTasks />
     </main>
   );
 };
