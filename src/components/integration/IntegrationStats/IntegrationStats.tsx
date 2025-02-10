e50a53fa209a8de66ac266cab6ac1fa208335d82
Valeriy <masterbox06@gmail.com>
2025-02-10 15:25:25 +0400

    chore: add abbreviation format wrapper at integrations and statistics pages

--- src/components/integration/IntegrationStats/IntegrationStats.tsx
+++ src/components/integration/IntegrationStats/IntegrationStats.tsx
@@ -5,6 +5,7 @@ import styles from './IntegrationStats.module.scss';
 import subscribersIcon from '../../../assets/icons/subscribers.png';
 import viewsIcon from '../../../assets/icons/views.png';
 import coinIcon from '../../../assets/icons/coin.png';
+import { formatAbbreviation } from '../../../helpers';
 
 interface IntegrationStatsProps {
     subscribers: number;
@@ -16,21 +17,21 @@ export const IntegrationStats: React.FC<IntegrationStatsProps> = ({subscribers, 
     return (
         <div className={styles.IntegrationStatsWrp}>
             <div className={styles.IntegrationStat}>
-                <p className={styles.amount}>{subscribers}</p>
+                <p className={styles.amount}>{formatAbbreviation(subscribers)}</p>
                 <div className={styles.typeWrp}>
                     <img src={subscribersIcon} width={12} height={12}/>
                     <p className={styles.type}>Подписчики</p>
                 </div>
             </div>
             <div className={styles.IntegrationStat}>
-                <p className={styles.amount}>{views}</p>
+                <p className={styles.amount}>{formatAbbreviation(views)}</p>
                 <div className={styles.typeWrp}>
                     <img src={viewsIcon} width={12} height={12}/>
                     <p className={styles.type}>Просмотры</p>
                 </div>
             </div>
             <div className={styles.IntegrationStat}>
-                <p className={styles.amount}>{income}</p>
+                <p className={styles.amount}>{formatAbbreviation(income)}</p>
                 <div className={styles.typeWrp}>
                     <img src={coinIcon} width={12} height={12}/>
                     <p className={styles.type}>Доход</p>
