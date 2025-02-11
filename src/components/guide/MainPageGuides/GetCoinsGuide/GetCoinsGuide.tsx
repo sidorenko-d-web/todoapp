43584cbd6a8e417edcba3c90c4661b81a67e8550
Kiryl Filipovich <fkiryl@gmail.com>
2025-02-11 15:09:26 +0300

    integrated is_invited param to get coins guide

--- src/components/guide/MainPageGuides/GetCoinsGuide/GetCoinsGuide.tsx
+++ src/components/guide/MainPageGuides/GetCoinsGuide/GetCoinsGuide.tsx
@@ -5,12 +5,15 @@ import img1 from '../../../../assets/gif/guide4.gif';
 import { Guide } from "../../Guide/Guide";
 
 import coin from '../../../../assets/icons/coin.png';
+import { useGetUserQuery } from "../../../../redux";
 
 interface GetCoinsGuideProps {
     onClose: () => void;
-    isReferral: boolean;
 }
-export const GetCoinsGuide: React.FC<GetCoinsGuideProps> = ({ onClose, isReferral }) => {
+export const GetCoinsGuide: React.FC<GetCoinsGuideProps> = ({ onClose }) => {
+
+    const { data } = useGetUserQuery();
+
     const [isOpen, setIsOpen] = useState(true);
 
     const handleClose = () => {
@@ -32,12 +35,12 @@ export const GetCoinsGuide: React.FC<GetCoinsGuideProps> = ({ onClose, isReferra
                     Держи 250 баллов для твоего блогерского старта!
                     <br />
                     <br />
-                    {isReferral
+                    {data?.is_invited
                          && <span>И еще <span style={{ color: '#EC7913' }}>150 баллов</span> ты получаешь от друга, который тебя пригласил!!</span>}
                 </>
             }
             onClose={onClose}>
-            <button className={styles.nextBtn} onClick={handleClose}>{`Забрать ${isReferral ? '400' : '250'}`}<img src={coin} width={14} height={14} /></button>
+            <button className={styles.nextBtn} onClick={handleClose}>{`Забрать ${data?.is_invited ? '400' : '250'}`}<img src={coin} width={14} height={14} /></button>
             <img src={img1} className={styles.gifImage} height={146} width={140} />
         </Guide>
     );
