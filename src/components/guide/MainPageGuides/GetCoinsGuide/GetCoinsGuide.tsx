4e4bf7186ffcad11d23c64c9db7746a3196517c2
Kiryl Filipovich <fkiryl@gmail.com>
2025-02-11 16:20:53 +0300

    integrated is_invited param to get coins guide

--- src/components/guide/MainPageGuides/GetCoinsGuide/GetCoinsGuide.tsx
+++ src/components/guide/MainPageGuides/GetCoinsGuide/GetCoinsGuide.tsx
@@ -24,24 +24,26 @@ export const GetCoinsGuide: React.FC<GetCoinsGuideProps> = ({ onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <Guide align="left"
-            zIndex={110}
-            top={'25%'}
-            description={
-                <>
-                    Ах, да... для этого нужно немного вложиться... 
-                    <br />
-                    <br />
-                    Держи 250 баллов для твоего блогерского старта!
-                    <br />
-                    <br />
-                    {data?.is_invited
-                         && <span>И еще <span style={{ color: '#EC7913' }}>150 баллов</span> ты получаешь от друга, который тебя пригласил!!</span>}
-                </>
-            }
-            onClose={onClose}>
-            <button className={styles.nextBtn} onClick={handleClose}>{`Забрать ${data?.is_invited ? '400' : '250'}`}<img src={coin} width={14} height={14} /></button>
-            <img src={img1} className={styles.gifImage} height={146} width={140} />
-        </Guide>
+        <>
+            {data && <Guide align="left"
+                zIndex={110}
+                top={'25%'}
+                description={
+                    <>
+                        Ах, да... для этого нужно немного вложиться...
+                        <br />
+                        <br />
+                        Держи 250 баллов для твоего блогерского старта!
+                        <br />
+                        <br />
+                        {data?.is_invited
+                            && <span>И еще <span style={{ color: '#EC7913' }}>150 баллов</span> ты получаешь от друга, который тебя пригласил!!</span>}
+                    </>
+                }
+                onClose={onClose}>
+                <button className={styles.nextBtn} onClick={handleClose}>{`Забрать ${data?.is_invited ? '400' : '250'}`}<img src={coin} width={14} height={14} /></button>
+                <img src={img1} className={styles.gifImage} height={146} width={140} />
+            </Guide>}
+        </>
     );
 };
