adbf8b9ec74a54362aa6513f765a9924e31b2055
Kiryl Filipovich <fkiryl@gmail.com>
2025-02-11 14:22:35 +0300

    subscribe 100 coins not 450

--- src/components/subscription/SubscribeModal/SubscribeModal.tsx
+++ src/components/subscription/SubscribeModal/SubscribeModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import CentralModal from '../../shared/CentralModal/CentralModal';
 import integrationWhiteIcon from '../../../assets/icons/integration-white.svg';
 import coinIcon from '../../../assets/icons/coin.png';
 import { useBuySubscriptionMutation } from '../../../redux';
