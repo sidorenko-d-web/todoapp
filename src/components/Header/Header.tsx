30a55a5f291665561f8c7a816a44b6245db60f82
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-02-15 12:01:27 +0300

    Fix layout add shifting settings icon to the center of content safe area inset top

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ import { useEffect } from 'react';
 import { setLastActiveStage } from '../../redux/slices/tree.ts';
 import { formatAbbreviation } from '../../helpers';
 import { useTranslation } from 'react-i18next';
-import { TrackedLink } from '../';
+import { TrackedLink } from '../withTracking';
 
 
 export const Header = () => {
@@ -22,7 +22,7 @@ export const Header = () => {
   const points = useSelector((state: RootState) => state.pointSlice.points);
   const lastActiveStage = useSelector((state: RootState) => state.treeSlice.lastActiveStage);
   const { i18n } = useTranslation('profile');
-  const locale = [ 'ru', 'en' ].includes(i18n.language) ? (i18n.language as 'ru' | 'en') : 'ru';
+  const locale = ['ru', 'en'].includes(i18n.language) ? (i18n.language as 'ru' | 'en') : 'ru';
 
   const userSubscribers = data?.subscribers || 0;
   let lastActiveStageNumber = 0;
@@ -36,11 +36,11 @@ export const Header = () => {
     if (lastActiveStageNumber) {
       dispatch(setLastActiveStage(lastActiveStageNumber));
     }
-  }, [ lastActiveStageNumber, dispatch ]);
+  }, [lastActiveStageNumber, dispatch]);
 
   useEffect(() => {
     refetch();
-  }, [ points ]);
+  }, [points]);
 
   const handleNavigateToProfile = () => {
     navigate(AppRoute.Profile);
@@ -51,41 +51,41 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       {isLoading ? <p>Loading...</p> :
-      <div className={styles.lowerHeader}>
-        <div className={styles.levelWrapper}>
-          <div className={styles.avatarWrapper} onClick={handleNavigateToProfile}>
-            <img className={styles.avatarIcon} src={AvatarIcon} alt="AvatarIcon" />
-            <img className={styles.fireIcon} src={FireIcon} alt="FireIcon" />
-          </div>
-
-          <div className={styles.info}>
-            <div className={styles.subscribers}>
-              <p
-                className={styles.subscribersNumber}>{formatAbbreviation(data?.subscribers || 0, 'number', { locale: locale })}</p>
-              <img className={styles.subscribersIcon} src={SubscribersIcon} alt="SubscribersIcon" />
+        <div className={styles.lowerHeader}>
+          <div className={styles.levelWrapper}>
+            <div className={styles.avatarWrapper} onClick={handleNavigateToProfile}>
+              <img className={styles.avatarIcon} src={AvatarIcon} alt="AvatarIcon" />
+              <img className={styles.fireIcon} src={FireIcon} alt="FireIcon" />
             </div>
 
-            <div className={styles.levelInfo}>
-              <TrackedLink
-                trackingData={{
-                  eventType: 'button',
-                  eventPlace: 'В дерево роста - Хедер',
-                }}
-                to={AppRoute.ProgressTree} className={styles.levelNumber}>
-                {lastActiveStage}
-              </TrackedLink>
-              <progress max={10} value={6} className={styles.levelProgressBar}></progress>
+            <div className={styles.info}>
+              <div className={styles.subscribers}>
+                <p
+                  className={styles.subscribersNumber}>{formatAbbreviation(data?.subscribers || 0, 'number', { locale: locale })}</p>
+                <img className={styles.subscribersIcon} src={SubscribersIcon} alt="SubscribersIcon" />
+              </div>
+
+              <div className={styles.levelInfo}>
+                <TrackedLink
+                  trackingData={{
+                    eventType: 'button',
+                    eventPlace: 'В дерево роста - Хедер',
+                  }}
+                  to={AppRoute.ProgressTree} className={styles.levelNumber}>
+                  {lastActiveStage}
+                </TrackedLink>
+                <progress max={10} value={6} className={styles.levelProgressBar}></progress>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={styles.coinsWrapper}>
-          <p
-            className={styles.coins}>{formatAbbreviation(showCoins ? (data?.points || 0) : '0', 'number', { locale: locale })}</p>
-          <img className={styles.coinIcon} src={CoinIcon} alt="CoinIcon" />
+          <div className={styles.coinsWrapper}>
+            <p
+              className={styles.coins}>{formatAbbreviation(showCoins ? (data?.points || 0) : '0', 'number', { locale: locale })}</p>
+            <img className={styles.coinIcon} src={CoinIcon} alt="CoinIcon" />
+          </div>
         </div>
-      </div>
-    }
+      }
     </header>
   );
 };
