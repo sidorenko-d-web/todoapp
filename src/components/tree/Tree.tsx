dcd1813354ce3e00130ab1d46fa9cdd20c71c0dd
Valeriy <masterbox06@gmail.com>
2025-02-20 16:49:19 +0400

    feat(tree): alternate gift colors, prize positions, and add question marks
    - Alternate gift colors in a blue-purple-red sequence
    - Alternate prize positions (left/right)
    - Add question marks to indicate varying prizes

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -3,12 +3,22 @@ import s from './Tree.module.scss';
 import classNames from 'classnames';
 import tickCircle from '../../assets/icons/tickCircle.svg';
 import circle from '../../assets/icons/circle.svg';
-import gift from '../../assets/icons/gift.svg';
-import spinner from '../../assets/icons/purple-glow.svg';
 import { useGetCurrentUserProfileInfoQuery, useGetTreeInfoQuery } from '../../redux';
 import { useTreeProgress } from '../../hooks';
 import { formatAbbreviation } from '../../helpers';
 import { useTranslation } from 'react-i18next';
+
+import giftBlue from '../../assets/icons/gift.svg';
+import giftPurple from '../../assets/icons/gift-purple.svg';
+import giftRed from '../../assets/icons/gift-red.svg';
+
+import questionBlue from '../../assets/icons/question-blue.svg';
+import questionPurple from '../../assets/icons/question-purple.svg';
+import questionRed from '../../assets/icons/question-red.svg';
+
+import spinnerPurple from '../../assets/icons/purple-glow.svg';
+import spinnerBlue from '../../assets/icons/blue-glow.svg';
+import spinnerRed from '../../assets/icons/red-glow.svg';
 
 export const Tree = () => {
   const { t, i18n } = useTranslation('tree');
@@ -22,7 +32,7 @@ export const Tree = () => {
 
   const { progressPercent } = useTreeProgress({
     treeData,
-    userSubscribers
+    userSubscribers,
   });
 
   useEffect(() => {
@@ -32,7 +42,7 @@ export const Tree = () => {
         block: 'center',
       });
     }
-  }, [progressPercent]);
+  }, [ progressPercent ]);
 
   if (!treeData) {
     return null;
@@ -51,34 +61,45 @@ export const Tree = () => {
             const isActive = userSubscribers >= stage.subscribers;
             const bottomPosition = 150 + index * 300;
 
+            const giftColors = [ giftBlue, giftPurple, giftRed ];
+            const questionColors = [ questionBlue, questionPurple, questionRed ];
+            const spinnerColors = [ spinnerBlue, spinnerPurple, spinnerRed ];
+
+            const giftIcon = giftColors[index % giftColors.length];
+            const questionIcon = questionColors[index % questionColors.length];
+            const spinnerIcon = spinnerColors[index % spinnerColors.length];
+
             return (
               <div key={stage.id} className={s.levelMarker} style={{ bottom: `${bottomPosition}px` }}>
                 <div className={classNames(s.levelCircle, { [s.active]: isActive })}>
                   {isActive ? (
-                    <img src={tickCircle} height={16} width={16} alt="tickCircle" style={{zIndex: '0'}}/>
+                    <img src={tickCircle} height={16} width={16} alt="tickCircle" style={{ zIndex: '0' }} />
                   ) : (
-                    <img src={circle} height={16} width={16} alt="circle" style={{zIndex: '0'}}/>
+                    <img src={circle} height={16} width={16} alt="circle" style={{ zIndex: '0' }} />
                   )}
                   {stage.stage_number}
-                {stage.stage_number % 10 === 0 && (
-                  <img className={s.spiner} src={spinner} height={120} width={120} alt="spinner" />
-                )}
+                  {stage.stage_number % 10 === 0 && (
+                    <img className={s.spiner} src={spinnerIcon} height={120} width={120} alt="spinner" />
+                  )}
                 </div>
                 {stage.id > 1 && (
                   <div
                     className={classNames(s.prize, {
                       [s.priseSubscribers]: !stage.achievement,
-                      [s.prizeActive]: isActive || stage.stage_number % 10 === 0,
+                      [s.prizeRight]: index % 2 !== 1,
                     })}
                   >
                     {stage.achievement && (
                       <div className={classNames(s.imgPrize, { [s.imgPrizeActive]: isActive })}>
                         <div className={classNames({ [s.blur]: !isActive })} />
-                        <img src={gift} height={20} width={20} alt="gift" />
+                        <img src={giftIcon} height={20} width={20} alt="gift" />
+                        {!isActive && <div className={classNames(s.questionWrapper)}>
+                          <img src={questionIcon} className={s.question} height={16} width={16} alt="question" />
+                        </div>}
                       </div>
                     )}
-                    <p className={classNames(s.text, { [s.textActive]: isActive})}>
-                      {formatAbbreviation(stage.subscribers, 'number', {locale: locale})} <br />
+                    <p className={classNames(s.text, { [s.textActive]: isActive })}>
+                      {formatAbbreviation(stage.subscribers, 'number', { locale: locale })} <br />
                       {t('t1')}
                     </p>
                   </div>
