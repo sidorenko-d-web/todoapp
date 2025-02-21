011246a53510adf7b8a3b4df5bce7472fc70ba09
Kiryl Filipovich <fkiryl@gmail.com>
2025-02-21 15:35:36 +0300

    merged main

--- src/hooks/AuthInit.tsx
+++ src/hooks/AuthInit.tsx
@@ -47,9 +47,6 @@ export function AuthInit({ children }: AuthInitProps) {
         const hasCompletedSetup = localStorage.getItem('hasCompletedSetup');
         const savedStep = localStorage.getItem('currentSetupStep') as AuthStep;
 
-        console.log('hasCompletedSetup:', hasCompletedSetup);
-        console.log('savedStep:', savedStep);
-
         //const init_data = window.Telegram.WebApp.initData;
 
         // const init_data = `query_id%3DAAHa-fEBAwAAANr58QFFg6SM%26user%3D%257B%2522id%2522%253A6475086298%252C%2522first_name%2522%253A%2522%25D0%2590%25D0%25BD%25D0%25B4%25D1%2580%25D0%25B5%25D0%25B9%2522%252C%2522last_name%2522%253A%2522%25D0%2593%25D0%25B5%25D1%2580%25D0%25B0%25D1%2581%25D0%25B8%25D0%25BC%25D0%25BE%25D0%25B2%2522%252C%2522username%2522%253A%2522De_Geras%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%252C%2522photo_url%2522%253A%2522https%253A%255C%252F%255C%252Ft.me%255C%252Fi%255C%252Fuserpic%255C%252F320%255C%252FGzJ9TEsOx7Oc1JJcHr-lzeFQB6alew8O_qoBdhO-6gGt7kZEiGJ6urTqgGlF9_Ye.svg%2522%257D%26auth_date%3D1739263999%26signature%3D6Nc44J8mpAyftuBwiEXHF3YyBtkbAptwj1YO0P99Dhmkpy1G1Nw8wxfR48B22hnqfGPnxxrf9vcSenT5ylpACg%26hash%3D90880b7f94adbd34cac6cd5d023fc5822c26b6d3036709f5dd60f547d2889a2c&tgWebAppVersion=7.2&tgWebAppPlatform=macos&tgWebAppThemeParams=%7B%22button_text_color%22%3A%22%23ffffff%22%2C%22text_color%22%3A%22%23ffffff%22%2C%22link_color%22%3A%22%23007aff%22%2C%22section_header_text_color%22%3A%22%23e5e5e5%22%2C%22button_color%22%3A%22%23007aff%22%2C%22subtitle_text_color%22%3A%22%23ffffff%22%2C%22bg_color%22%3A%22%23282828%22%2C%22accent_text_color%22%3A%22%23007aff%22%2C%22secondary_bg_color%22%3A%22%231c1c1c%22%2C%22destructive_text_color%22%3A%22%23ff453a%22%2C%22header_bg_color%22%3A%22%231c1c1c%22%2C%22section_bg_color%22%3A%22%23282828%22%2C%22hint_color%22%3A%22%23ffffff%22%7D`;
@@ -102,20 +99,22 @@ export function AuthInit({ children }: AuthInitProps) {
   };
 
   const handleLanguageContinue = () => {
-    saveCurrentStep('skin');
-  };
-
-  const handleSkinContinue = () => {
     saveCurrentStep('invite_code');
   };
 
-
-  const handleInviteCodeContinue = () => {
-    console.log('Invite code entered, continuing...');
+  const handleSkinContinue = () => {
     localStorage.setItem('hasCompletedSetup', 'true');
     localStorage.removeItem('currentSetupStep');
 
     saveCurrentStep('completed');
+  };
+
+
+  const handleInviteCodeContinue = () => {
+    localStorage.setItem('hasCompletedSetup', 'true');
+    localStorage.removeItem('currentSetupStep');
+
+    saveCurrentStep('skin');
   };
 
 
@@ -139,12 +138,11 @@ export function AuthInit({ children }: AuthInitProps) {
         />
       );
 
+    case 'invite_code':
+      return <EnterInviteCodePage onContinue={handleInviteCodeContinue} />;
+
     case 'skin':
       return <SkinSetupPage onContinue={handleSkinContinue} />;
-
-    case 'invite_code':
-      console.log('Rendering Invite Code Page...');
-      return <EnterInviteCodePage onContinue={handleInviteCodeContinue} />;
 
     case 'completed':
       return <>{children}</>;
