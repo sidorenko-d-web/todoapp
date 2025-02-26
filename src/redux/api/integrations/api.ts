4084eb585f228c83054fa56208386b6d190a4f64
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-02-26 14:53:09 +0300

    Edit redux integrations api

--- src/redux/api/integrations/api.ts
+++ src/redux/api/integrations/api.ts
@@ -61,17 +61,10 @@ export const integrationsApi = createApi({
       },
       providesTags: ['Integrations'],
     }),
-    getAllIntegrations: builder.query<IntegrationsResponseDTO, IntegrationsQueryRequestDTO>({
-      query: ( params ) => ({
+    getAllIntegrations: builder.query<IntegrationsResponseDTO, void>({
+      query: () => ({
         url: '/integrations',
         method: 'GET',
-        params: {
-          company_name: params.company_name,
-          status: params.status,
-          asc: params.asc,
-          offset: params.offset,
-          limit: params.limit
-        }
       }),
     }),
     updateTimeLeft: builder.mutation<IntegrationResponseDTO, IntegrationUpdateRequestDTO>({
