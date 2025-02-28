f33a4fdbafa582de5b8770fae2587a34555185d7
marketerKA <karhipov606@gmail.com>
2025-02-28 23:04:15 +0300

    Update: Part #1 new animation, fix text style, position style

--- src/assets/animations/imports.ts
+++ src/assets/animations/imports.ts
@@ -2,9 +2,11 @@ import blue from './blueLight.json';
 import purple from './purpleLight.json';
 import red from './redLight.json';
 import hand from './hand.json';
+import coin from './coinLoading.json';
 
 
 export const blueLight = blue;
 export const redLight = red;
 export const purpleLight = purple;
 export const handAnimation = hand;
+export const coinLoadingAnimation = coin;
