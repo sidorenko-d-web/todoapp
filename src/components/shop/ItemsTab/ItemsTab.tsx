020c349f980969976646713c11e18fa2267046d2
Kiryl Filipovich <fkiryl@gmail.com>
2025-03-08 23:43:25 +0300

    fixing guides

--- src/components/shop/ItemsTab/ItemsTab.tsx
+++ src/components/shop/ItemsTab/ItemsTab.tsx
@@ -23,19 +23,17 @@ export const ItemsTab: FC<props> = ({ inventoryItems, shopItems }) => {
       {inventoryItems?.map(item => (
         <InventoryCard key={item.id} item={item} />
       ))}
-      {/* todo: remove before prod */}
-      {/* {isGuideShown(GUIDE_ITEMS.shopPage.BACK_TO_MAIN_PAGE_GUIDE) ? ( */}
-      {/* {isGuideShown(GUIDE_ITEMS.shopPage.BACK_TO_MAIN_PAGE_GUIDE) ? (
-        <> */}
+      {isGuideShown(GUIDE_ITEMS.shopPage.BACK_TO_MAIN_PAGE_GUIDE) ? (
+        <>
           {shopItems?.map(item => (
             <ShopItemCard key={item.id} item={item} />
           ))}
-        {/* </>
+        </>
       ) : foundItem ? (
         <ShopItemCard key={foundItem.id} item={foundItem} />
       ) : (
         <></>
-      )} */}
+      )}
     </div>
   );
 };
