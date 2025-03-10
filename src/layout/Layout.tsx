2891b39c9035fa3c09ef769df67caf5a30066a6a
Valeriy <masterbox06@gmail.com>
2025-03-10 20:52:51 +0400

    chore: fix bg parallax effect

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,16 +7,15 @@ import { localStorageConsts, MODALS } from '../constants';
 import { LanguageSelectionModal, Settings, SettingsModal, WalletConnectionModal } from '../components';
 import { useModal, useScrollManager } from '../hooks';
 import { getOS } from '../utils';
-
-import roadmapBg from '../assets/pages-bg/roadmap-bg.png';
 import Lottie from 'lottie-react';
 import { lampTable } from '../assets/animations';
+import roadmapBg from '../assets/pages-bg/roadmap-bg.png';
 
 const Layout = () => {
   const location = useLocation();
   const platform = getOS();
   const { openModal } = useModal();
-  const [bgOffset, setBgOffset] = useState(0);
+  const [ bgOffset, setBgOffset ] = useState(0);
   const contentRef = useScrollManager();
 
   const showHeader = !location.pathname.match(/^\/profile\/[0-9a-fA-F-]{36}$/);
@@ -24,7 +23,7 @@ const Layout = () => {
   const needsReducedMargin = [
     '/',
     '/progressTree',
-    location.pathname.match(/^\/profile\/[0-9a-fA-F-]{36}$/)
+    location.pathname.match(/^\/profile\/[0-9a-fA-F-]{36}$/),
   ].includes(location.pathname);
 
   const showRoadmapBg = location.pathname === '/progressTree';
@@ -37,8 +36,8 @@ const Layout = () => {
   }, []);
 
   const contentClassName = `${styles.content} ${showHeader ? styles.withHeader : ''
-    } ${needsReducedMargin ? styles.reducedMargin : ''} ${platform ? styles[platform] : ''
-    }`;
+  } ${needsReducedMargin ? styles.reducedMargin : ''} ${platform ? styles[platform] : ''
+  }`;
 
   useEffect(() => {
     if (showRoadmapBg && contentRef.current) {
@@ -46,21 +45,21 @@ const Layout = () => {
         const scrollTop = contentRef.current?.scrollTop || 0;
         const maxScroll = (contentRef.current?.scrollHeight || 0) - (contentRef.current?.clientHeight || 0);
         const scrollPercentage = maxScroll > 0 ? scrollTop / maxScroll : 0;
-        const newOffset = scrollPercentage * 100;
+        const newOffset = scrollPercentage * 75;
         setBgOffset(newOffset);
       };
 
-      contentRef.current.addEventListener("scroll", handleScroll);
+      contentRef.current.addEventListener('scroll', handleScroll);
       return () => {
-        contentRef.current?.removeEventListener("scroll", handleScroll);
+        contentRef.current?.removeEventListener('scroll', handleScroll);
       };
     }
-  }, [showRoadmapBg, contentRef.current]);
+  }, [ showRoadmapBg, contentRef.current ]);
 
   return (
     <>
       <div className={`${styles.settingsIcon} ${platform ? styles[platform + 'Settings'] : ''
-        }`}>
+      }`}>
         <Settings />
       </div>
       <div className={styles.wrp}>
@@ -69,7 +68,7 @@ const Layout = () => {
             <img
               src={roadmapBg}
               className={styles.bg_image}
-              style={{ transform: `translateY(-${bgOffset}px)` }}
+              style={{ transform: `translateY(-${bgOffset}%)` }}
             />
             <Lottie
               animationData={lampTable}
