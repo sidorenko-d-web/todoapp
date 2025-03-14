fdca8c1e0cfe17c577dc46ffbc208f4f1fa586e5
DeHopen <a2302004b23@yandex.ru>
2025-03-14 10:31:58 +0300

    Fix: delete icons in settings

--- src/components/settings/SettingsModal/SettingsModal.tsx
+++ src/components/settings/SettingsModal/SettingsModal.tsx
@@ -1,15 +1,12 @@
 import styles from './SettingsModal.module.scss';
 import { MODALS } from '../../../constants';
 import { useModal } from '../../../hooks';
-import russiaIcon from '../../../assets/icons/ru-flag.svg';
-import usaIcon from '../../../assets/icons/us-flag.svg';
-import cryptoWalletIcon from '../../../assets/icons/Wallet.png';
 import ArrowRight from '../../../assets/icons/arrow-right.svg';
 import { useTranslation } from 'react-i18next';
 import { CentralModal } from '../../shared';
 
 export const SettingsModal = () => {
-  const { t, i18n } = useTranslation('settings');
+  const { t } = useTranslation('settings');
   const { closeModal, openModal } = useModal();
 
   const handleCloseModal = () => {
@@ -26,8 +23,6 @@ export const SettingsModal = () => {
     // closeModal(MODALS.SETTINGS)
   };
 
-  const currentLanguageIcon = i18n.language === 'ru' ? russiaIcon : usaIcon;
-
   return (
     <CentralModal
       modalId={MODALS.SETTINGS}
@@ -38,7 +33,6 @@ export const SettingsModal = () => {
       <div className={styles.wrapper}>
         <div className={styles.childModalWrapper} onClick={handleOpenLanguageSelectionModal}>
           <div className={styles.titleAndIcon}>
-            <img className={styles.icon} src={currentLanguageIcon} alt="" />
             {t('s2')}
           </div>
           <img className={styles.arrow} src={ArrowRight} alt="" />
@@ -46,9 +40,6 @@ export const SettingsModal = () => {
 
         <div className={styles.childModalWrapper} onClick={handleOpenWalletConnectionModal}>
           <div className={styles.titleAndIcon}>
-            <div>
-              <img className={styles.icon} src={cryptoWalletIcon} alt="" />
-            </div>
             {t('s3')}
           </div>
           <img className={styles.arrow} src={ArrowRight} alt="" />
