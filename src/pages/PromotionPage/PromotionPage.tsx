70676dadfab78d7ea062731d82a38d2db7b51b68
DeHopen <a2302004b23@yandex.ru>
2025-03-15 11:02:12 +0300

    Fix: after merge

--- src/pages/PromotionPage/PromotionPage.tsx
+++ src/pages/PromotionPage/PromotionPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import subscribersIcon from '../../assets/icons/subscribers.png';
 import { DevelopmentPlan, IncreaseIncome, Loader, TopInfluencers } from '../../components';
 
