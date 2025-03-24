4c7525e44a8962a368ef9ba53404400de8ff7e1a
Kiryl Filipovich <fkiryl@gmail.com>
2025-03-24 14:43:11 +0300

    tree level guide fix

--- src/layout/ShopLayout/ShopLayout.tsx
+++ src/layout/ShopLayout/ShopLayout.tsx
@@ -298,13 +298,14 @@ export const ShopLayout: FC<PropsWithChildren<Props>> = ({
       {isGuideShown(GUIDE_ITEMS.integrationPage.INTEGRATION_PAGE_GUIDE_SHOWN) &&
         isGuideShown(GUIDE_ITEMS.mainPageSecondVisit.FINISH_TUTORIAL_GUIDE_SHOWN) &&
         !isGuideShown(GUIDE_ITEMS.shopPageSecondVisit.UPGRADE_ITEMS_GUIDE_SHOWN) &&
-        mode === 'shop' && (
+        mode === 'shop' && showWelcomeGuide && (
           <UpgradeItemsGuide
             onClose={() => {
               setGuideShown(GUIDE_ITEMS.shopPageSecondVisit.UPGRADE_ITEMS_GUIDE_SHOWN);
               setGuideShown(GUIDE_ITEMS.shopPageSecondVisit.UPGRADE_ITEMS_GUIDE_SHOWN);
               //dispatch(setDimHeader(false));
               setRerender(prev => prev + 1);
+              navigate(AppRoute.ShopInventory);
             }}
           />
         )}
