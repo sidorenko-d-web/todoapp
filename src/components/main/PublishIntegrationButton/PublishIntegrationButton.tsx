e9cc8ab4433c1bfc05c513d4b4ba40ef5c168a40
Valeriy <masterbox06@gmail.com>
2025-04-01 14:12:19 +0400

    chore: lower gift position when there are no integrations to create

--- src/components/main/PublishIntegrationButton/PublishIntegrationButton.tsx
+++ src/components/main/PublishIntegrationButton/PublishIntegrationButton.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import integrationIcon from '../../../assets/icons/integration.svg';
 import { useModal } from '../../../hooks';
 import { MODALS } from '../../../constants/modals.ts';
 import {
   RootState,
-  useGetAllIntegrationsQuery,
   useGetIntegrationQuery,
   useGetIntegrationsQuery,
   usePublishIntegrationMutation,
@@ -30,7 +29,7 @@ export const PublishIntegrationButton: React.FC = () => {
   const isPublishedModalClosed = useSelector((state: RootState) => state.guide.isPublishedModalClosed);
 
   const [publishIntegration] = usePublishIntegrationMutation();
-  const { data: allIntegrations, refetch } = useGetAllIntegrationsQuery();
+  const { data: allIntegrations, refetch } = useGetIntegrationsQuery();
   const [isPublishing, setIsPublishing] = useState(false);
   const [isTimeUpdating, setIsTimeUpdating] = useState(false);
   const [updateTimeLeft] = useUpdateTimeLeftMutation();
