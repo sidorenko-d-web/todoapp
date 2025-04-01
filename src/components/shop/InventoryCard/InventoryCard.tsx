78092b060b0cd83a18383211532fffcef26916a9
DeHopen <a2302004b23@yandex.ru>
2025-04-02 01:45:42 +0300

    Fix: after merge

--- src/components/shop/InventoryCard/InventoryCard.tsx
+++ src/components/shop/InventoryCard/InventoryCard.tsx
@@ -27,7 +27,6 @@ import {
   buildLink,
   buildMode,
   GUIDE_ITEMS,
-  itemStoreString,
   localStorageConsts,
   MODALS,
   SOUNDS,
--- src/hooks/Auth/useAuthFlow.ts
+++ src/hooks/Auth/useAuthFlow.ts
@@ -3,7 +3,7 @@ import { useSignInMutation } from '../../redux';
 import { useTranslation } from 'react-i18next';
 import { performSignIn } from './authService';
 import { AuthStep } from './typesAuth.ts';
-import { buildMode } from '../../constants/config.ts';
+// import { buildMode } from '../../constants/config.ts';
 // import { useModal } from '../useModal.ts';
 // import { MODALS } from '../../constants';
 
