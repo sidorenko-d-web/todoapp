88dac6fa1d9023efd1a7b5b849aafcc13eb63dea
Viktor <viktor4strlsov@gmail.com>
2025-04-06 01:32:36 +0600

    fix: transition from integration to store

--- src/components/shared/LoadingScreen/LoadingScreen.tsx
+++ src/components/shared/LoadingScreen/LoadingScreen.tsx
@@ -55,19 +55,19 @@ export const LoadingScreen = ({ onAnimationComplete, isAuthComplete }: LoadingSc
   const [isMobile, setIsMobile] = useState(buildMode.includes('Dev') ? 1 : 0);
   // const [isMobile, setIsMobile] = useState(1);
 
-  useEffect(() => {
-    if (buildMode.includes('Dev')) return;
-    if (window.Telegram?.WebApp?.platform) {
-      const platform = window.Telegram.WebApp.platform.toLowerCase();
-      if (platform.includes('android') || platform.includes('ios')) {
-        setIsMobile(1);
-      } else {
-        setIsMobile(-1);
-      }
-    } else {
-      setIsMobile(-1);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (buildMode.includes('Dev')) return;
+  //   if (window.Telegram?.WebApp?.platform) {
+  //     const platform = window.Telegram.WebApp.platform.toLowerCase();
+  //     if (platform.includes('android') || platform.includes('ios')) {
+  //       setIsMobile(1);
+  //     } else {
+  //       setIsMobile(-1);
+  //     }
+  //   } else {
+  //     setIsMobile(-1);
+  //   }
+  // }, []);
 
   useEffect(() => {
     const minLoadingTimeout = setTimeout(() => {
