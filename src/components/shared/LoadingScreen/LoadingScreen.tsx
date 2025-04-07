49f1815058a92c4f4683d3c5ab0a829bac1d0820
marketerKA <karhipov606@gmail.com>
2025-04-07 23:36:14 +0300

    Update: unable to see app in desktop (testDev, test)

--- src/components/shared/LoadingScreen/LoadingScreen.tsx
+++ src/components/shared/LoadingScreen/LoadingScreen.tsx
@@ -137,7 +137,7 @@ export const LoadingScreen = ({ onAnimationComplete, isAuthComplete }: LoadingSc
   return (
     <>
       <WhiteNoiseCanvas />
-      {isMobile === 1 && (
+      {(isMobile === 1 || buildMode === 'testDev' || buildMode === 'test') &&(
         <div className={styles.root} onClick={handleAccelerate}>
           <div />
           <div className={styles.clickableArea}></div>
@@ -173,7 +173,7 @@ export const LoadingScreen = ({ onAnimationComplete, isAuthComplete }: LoadingSc
         </div>
       )}
 
-      {isMobile === -1 && (
+        {isMobile === -1 && buildMode !== 'testDev' && buildMode !== 'test' &&  (
         <div className={styles.notMobile}>
           <div className={styles.qr}>
             <img src={qr} />
